14ec5bf91c5d408cd18a52f85a1ce549ef82db00
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-03-29 14:37:18 -0300

    Ejercicios 1 a 3

---
+++ Assets/Scripts/Jero1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jero1 : MonoBehaviour
+{
+    public int num1 = 4;
+    public int num2 = 5;
+    public string operacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Qué operación queres que haga?");
+        if (operacion == "Suma" || operacion == "suma")
+        {
+            Debug.Log(num1 += num2);
+        }
+
+        else if (operacion == "Producto" || operacion == "producto")
+        {
+            Debug.Log(num1 *= num2)                     ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Jero2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jero2 : MonoBehaviour
+{
+    public string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Buenos días/noches, " + nombre + "!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
